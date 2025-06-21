329ddc5bb6d237704c2e5c510d75b6e5feedb4d6
Anugrah Singhal <anugrah.singhal@okta.com>
2025-06-22 02:38:37 +0530

    feat: add config related to work profile

--- minimal/home-manager-configs/modules/shell.nix
+++ minimal/home-manager-configs/modules/shell.nix
@@ -3,19 +3,27 @@
   programs.zsh = {
     enable = true;
     enableCompletion = true;
+    initExtraBeforeCompInit = ''
+      # shell speed optimizations
+      for profile in ''${(z)NIX_PROFILES}; do
+        fpath[$fpath[(i)$profile/share/zsh/site-functions]]=()
+        fpath[$fpath[(i)$profile/share/zsh/$ZSH_VERSION/functions]]=()
+        fpath[$fpath[(i)$profile/share/zsh/vendor-completions]]=()
+      done
+    '';
     initContent = ''
       export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
-
-      source $HOME/.zshrc
 
       eval "$(/opt/homebrew/bin/brew shellenv)"
 
       source $HOME/.atuin.zsh
-      source "$HOME/.iterm2_shell_integration.zsh"
+      source $HOME/.iterm2_shell_integration.zsh
+
+      source $HOME/.zshrc
     '';
     autocd = true;
     # zprof.enable = true;
-    dotDir = ".config/zsh";
+    oh-my-zsh.enable = true; # over-ridden to false in work
   };
 
   # to remove the this you can add a line in ~/.zshenv to `source ${dotDir}/.zshenv`
