9f79d88518ab8db5e3c01a6a2dc042be3928f82f
Anugrah Singhal <anugrah.singhal@okta.com>
2025-06-22 02:38:37 +0530

    feat: add config related to work profile

--- minimal/home-manager-configs/work.nix
+++ minimal/home-manager-configs/work.nix
@@ -15,7 +15,7 @@
 
   home.shellAliases = {
     home-manager-switch = "cd ~/.config/minimal;git add .;sudo nix run . -- switch --flake .#work;cd -";
-    rbrew = "/opt/homebrew/bin/brew";
+    rbrew = "/opt/homebrew/bin/brew"; # real brew to bypass ocm brew
   };
 
   programs.zsh = {
@@ -24,12 +24,30 @@
       function sudo() {
         admin=$(groups | grep -q -w admin &> /dev/null)
         if [ $? -ne 0 ]; then
-              echo "custom sudo elevate"
-              jamfconnect acc-promo --elevate
+          echo "custom sudo elevate"
+          jamfconnect acc-promo --elevate
         fi
         /usr/bin/sudo "$@"
       }
     '';
+    dotDir = ".zsh-config-hm";
+    oh-my-zsh.enable = lib.mkForce false; # disable oh-my-zsh on work machine
   };
+
+
+  home.file = {
+    # to remove the this you can add a line in ~/.zshenv to `source ${dotDir}/.zshenv`
+    # we will use this to source the zshenv file manually in our config
+    ".zdotdir".text = ''
+      export HM_ZDOTDIR="$HOME/${config.programs.zsh.dotDir}"
+    '';
+    # add these lines to your ~/.zshrc
+    # source ~/.zdotdir
+    # source $HM_ZDOTDIR/.zshenv
+
+    # disable the default zshenv file created by home-manager and go manually source it in our config
+    ".zshenv".enable = false;
+  };
+
 
 }
