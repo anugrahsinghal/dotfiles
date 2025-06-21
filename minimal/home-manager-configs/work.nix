64c1762a17b6a8c07dd395a5a70292dd129a5be4
Anugrah Singhal <anugrah.singhal@okta.com>
2025-06-21 21:13:59 +0530

    feat: add nix-index to apps and enhance shell aliases in work configuration

--- minimal/home-manager-configs/work.nix
+++ minimal/home-manager-configs/work.nix
@@ -12,4 +12,23 @@
   home.packages = with pkgs; [
     go
   ];
+
+  home.shellAliases = {
+    home-manager-switch = "cd ~/.config/minimal;git add .;sudo nix run . -- switch --flake .#work;cd -";
+  };
+
+  programs.zsh = {
+    initContent = ''
+      # cusom sudo function 
+      function sudo() {
+        admin=$(groups | grep -q -w admin &> /dev/null)
+        if [ $? -ne 0 ]; then
+              echo "custom sudo elevate"
+              jamfconnect acc-promo --elevate
+        fi
+        /usr/bin/sudo "$@"
+      }
+    '';
+  };
+
 }
